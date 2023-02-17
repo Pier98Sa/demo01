c3ac47d9fb9ee9fd66d015d836026ed791ca1c59
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2023-02-17 10:48:36 +0100

    aggiunti esempi

--- src/app/c1/c1.component.ts
+++ src/app/c1/c1.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-c1',
   templateUrl: './c1.component.html',
-  styleUrls: ['./c1.component.css']
+  styleUrls: ['./c1.component.css'],
 })
 export class C1Component {
 
+  messaggio = "Ciao Angular";
+  provaVar = "Contenuto dell'input";
+  state = true;
+  
+  constructor() {
+    console.log('Il componente è stato creato dal runtime Angular');
+  }
+
+  ngOnInit(){
+    console.log('Il componente è stato inizializzato dal runtime Angular');
+  }
+
+  ngOnDestroy(){
+    console.log('Il componente è stato distrutto dal runtime Angular');
+  }
+
+  clickHandler(){
+    this.messaggio = "Premuto bottone";
+    this.provaVar =(this.state) ?  this.provaVar.toLowerCase() : this.provaVar.toUpperCase();
+    this.state = !this.state;
+  }
 }
